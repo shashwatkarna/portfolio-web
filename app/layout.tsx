1e4804d3e41809615d51bd64cbe66b5409dc13a7
Shashwat Karna <shashwatkarna6@gmail.com>
2025-06-12 12:11:14 +0530

    after deployment changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
